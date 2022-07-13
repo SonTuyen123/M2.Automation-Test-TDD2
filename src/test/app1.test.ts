a004061913969d421ccc31d114e7e5a57699d769
Admin <hanhngao@gmail.com>
2022-07-13 09:55:37 +0700

    add

---
+++ src/test/app1.test.ts
@@ -0,0 +1,27 @@
+import {AbsoluteNumberCalculator} from "../[Thực hành] Tìm giá trị tuyệt đối/app";
+describe("testFindAbsolute0", () => {
+    test("testFindAbsolute0", () => {
+        let number = 0;
+        let expected = 0;
+
+        expect(AbsoluteNumberCalculator.findAbsolute(number)).toEqual(expected);
+    });
+});
+
+describe("testFindAbsolute1", () => {
+    test("testFindAbsolute1", () => {
+        let number = 1;
+        let expected = 1;
+
+        expect(AbsoluteNumberCalculator.findAbsolute(number)).toEqual(expected);
+    });
+});
+
+describe("testFindAbsoluteNegative1", () => {
+    test("testFindAbsoluteNegative1", () => {
+        let number = -1;
+        let expected = 1;
+
+        expect(AbsoluteNumberCalculator.findAbsolute(number)).toEqual(expected);
+    });
+});
